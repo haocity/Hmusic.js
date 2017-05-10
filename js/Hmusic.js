e5eee95f9ef9b4f1a01c89146456c7509a7e83a9
皓东haocity <ureygt@gmail.com>
2017-05-10 19:49:42 +0800

    完成歌单解析

--- js/Hmusic.js
+++ js/Hmusic.js
@@ -1,12 +1,175 @@
-var e='.warp{margin:0 auto;width:340px}.hmusic{height:500px;box-shadow:0 0 5px rgba(0,0,0,.8);font-family:Avenir Next,Helvetica,Arial,Lantinghei SC,Microsoft YaHei,sans-serif}.hmusic>.banner{height:200px;background-image:url(1.jpg);background-position:center;background-size:cover}.hmusic>.banner>.banner-zz{overflow:hidden;height:100%;background-color:rgba(0,0,0,.4);color:#fff}.hmusic>.banner>.banner-zz>.songname{padding-top:5px;height:25px;text-align:center;font-size:1.2em}.hmusic>.banner>.banner-zz>.lrc-warp{overflow:hidden;height:10pc}.hmusic>.banner>.banner-zz>.lrc-warp>.lrc{text-align:center;transition:all .3s ease-out;transform:translateY(-10px)}.hmusic>.banner>.banner-zz>.lrc-warp>.lrc>p{margin:0;padding:0;line-height:30px}.hmusic>.banner>.banner-zz>.ranger{height:9pt;background-color:rgba(59,65,67,.39)}.hmusic>.banner>.banner-zz>.ranger>.ranger-n{width:20%;height:100%;background-color:rgba(3,169,244,.6)}.hmusic>.con{overflow:hidden;height:40px;text-align:center;user-select:none}.hmusic>.con .iconfont{color:#6d95e0;font-size:1.6em;cursor:pointer}.hmusic>.con .iconfont:hover{color:#2753af}.hmusic>.con>.icon-play,.hmusic>.con>.icon-stop{position:relative;top:3px;font-size:2em}.hmusic>.con>.right{float:right;padding:0 4px}.hmusic>.con>.left{float:left;padding:0 4px}.hmusic>.con>.left>.nowtime,.hmusic>.con>.right>.alltime{position:relative;top:-1pc;display:inline-block;font-size:.6em}.hmusic>.con>.right>.iconfont{padding-right:30px;line-height:1.6em}.hmusic>.con>.left>.iconfont{padding-left:30px;line-height:1.6em}.hmusic>.longarr{overflow-x:hidden;overflow-y:scroll;height:260px;background-color:#fafafa}.hmusic>.longarr>li{padding:3px 0 3px 20px;cursor:pointer}.hmusic>.longarr>li:hover{padding:3px 0 3px 24px;background-color:hsla(0,0%,69%,.9)}';
+var e='';
 var e2= document.createElement('style');
 e2.type="text/css";
 e2.innerHTML=e;
+//ajax
+var gc = gc || {};
+gc.ajax = function(options){//options =  {url:'',method:'',data:'',success:'',async:''}
+    //默认参数
+    options.url = options.url || '',
+    options.method = options.method || 'get',
+    options.data = options.data || '',
+    options.success = options.success || '',
+    options.error = options.error || function(e){console.log(e)},
+    options.async = options.async || true;
+    //get请求-拼接url
+    console.log(options.method.toLowerCase());
+    if(options.method.toLowerCase() == 'get'){
+        if(typeof options.data == 'object'){
+            options.data = [];
+            for (var k in options.data){
+                options.data.push(k+'='+options.data[k]);
+                options.data.join('&');
+            }
+        }
+        options.url += (options.url.indexOf('?' == -1) ? '?' : '') + options.data;
+    }
+    //post请求-转换字符串
+    if(options.method.toLowerCase() == 'post'){
+        if(typeof options.data == 'object'){
+            var arrs = [];
+            for (var k in options.data){
+                arrs.push(k+'='+options.data[k]);
+            }
+            options.data = arrs.join('&');
+        }
+    }
+    //创建发送请求
+    var xhr = window.XMLHttpRequest ?  new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP'); //兼容ie
+    xhr.open(options.method,options.url,options.async);
+    if(options.method == 'post'){
+        xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+        xhr.send(options.data);
+    }else{
+        xhr.send(null);
+    }
+    //异步请求
+    if(options.async == true){
+        xhr.onreadystatechange = function(){
+          if(xhr.readyState == 4){
+              callcall();
+          }
+        }
+    }
+    // xhr.abort(); // 取消异步请求
+    //同步请求
+    if(options.async == false){
+        callcall();
+    }
+    //返回状态判断
+    function callcall(){
+        if(xhr.status == 200){
+            options.success(xhr.responseText);
+        }else{
+            options.error('error:' + xhr.status + xhr.statusText);
+        }
+    }
+};
 document.head.appendChild(e2);
+var yl=new Object;
+function hyplaylist(ele,id){
+	yl.ele=ele;
+	yl.arr=new Array;
+	 var api='https://api.imjad.cn/cloudmusic';
+	 gc.ajax({
+	        url:api+'/?type=playlist&id='+id+'&br=128000',
+	        success:function(res){
+	            t=JSON.parse(res).playlist.tracks;
+	            for (var i = 0; i < t.length; i++) {
+	            	var c=new Object;
+	            	var au='';
+	            	c.yunid=t[i].id;
+	            	c.img=t[i].al.picUrl+'?param=320y320';
+	            	c.lrc=api+'/?type=lyric&id='+c.yunid+'&br=128000';
+	            	if(t[i].ar.length>4){
+		               au='群星';
+		               c.title=t[i].al.name;
+		            }else{
+		                for (var x = 0; x < t[i].ar.length; x++) {
+		                    au+=t[i].ar[x].name
+		                }
+		                c.title=t[i].name+'-'+au;
+		            }
+		            yl.arr.push(c);
+	            }
+	            console.log('加载歌单成功 正在解析各个音频地址');
+	            hyunmusic(yl.ele,yl.arr,1);
+	        }
+	    });
+	
+}
+
+function hyunmusic(ele,arr,one){
+	var ym=new Object;
+	ym.arr=arr;
+	ym.ele=ele;
+	ym.yuncloud=function(arr){
+	    for (var i = 0; i < arr.length; i++) {
+	       ym.getmusicutl(arr[i].yunid,i,one)
+	    };
+	}
+	ym.getmusicutl=function(id,i,one){
+	    var api='https://api.imjad.cn/cloudmusic';
+	    ym.arr[i].lrc=api+'/?type=lyric&id='+id+'&br=128000';
+	    gc.ajax({
+	    url:api+'/?type=song&id='+id+'&br=128000',
+	    success:function(res){
+	        t=JSON.parse(res).data[0].url;
+	        if(!ym.arr[i].audio){ym.arr[i].audio=t}
+	        ym.checksuccess(ym.arr);
+	    },
+	    error:function(e){
+	    	console.log(e);
+	    	setTimeout(function(){ym.getmusicutl(id,i)},1000)
+	    }
+	    });
+	    if(!one){
+	    	gc.ajax({
+	        url:api+'/?type=detail&id='+id+'&br=128000',
+	        success:function(res){
+	            t=JSON.parse(res).songs[0];
+	            var au='';
+	            if(t.ar.length>4){
+	               au='群星'
+	            }else{
+	                for (var x = 0; x < t.ar.length; x++) {
+	                    au+=t.ar[x].name
+	                }
+	            }
+	            ym.arr[i].img=t.al.picUrl+'?param=320y320';
+	            ym.arr[i].au=au;
+	            if(!ym.arr[i].title){
+	            	ym.arr[i].title=t.name+'-'+ym.arr[i].au;
+	            }
+	            ym.checksuccess(ym.arr);
+	        },
+	        error:function(e){
+	    	console.log(e);
+	    	setTimeout(function(){ym.getmusicutl(id,i)},1000)
+		    }
+		    });
+	    }
+
+	}
+	ym.checksuccess=function(arr){
+		var t=0;
+		for (var i = 0; i < arr.length; i++) {
+			if(arr[i].img&&arr[i].audio&&arr[i].title) {t++;}
+		}
+		if(t==arr.length){
+			console.log('yun music success');
+			Hmusic(ym.ele,arr)
+		}else{
+			console.log('没有完成');
+		}
+	}
+	ym.yuncloud(ym.arr);
+}
+
 function Hmusic(ele,arr){
 		function $c(c){return ele.querySelector(c)};
-		var hmele='<div class="hmusic"><audio class="hm-audio"></audio><div class="banner"><div class="banner-zz"><div class="songname"></div><div class="lrc-warp"><div class="lrc"></div></div><div class="ranger"><div class="ranger-n"></div></div></div></div><div class="con"><div class="left"><div class="nowtime">0:00</div><i class="iconfont icon-s"></i></div><i class="iconfont icon-play"></i><i class="iconfont icon-stop" style="display: none;"></i><div class="right"><i class="iconfont icon-x"></i><div class="alltime">1:00</div></div></div><div class="longarr"></div><style type="text/css" class="css"></style></div>';
-		ele.innerHTML=hmele;
+//		var hmele='<div class="hmusic"><audio class="hm-audio"></audio><div class="banner"><div class="banner-zz"><div class="songname"></div><div class="lrc-warp"><div class="lrc"></div></div><div class="ranger"><div class="ranger-n"></div></div></div></div><div class="con"><div class="left"><div class="nowtime">0:00</div><i class="iconfont icon-s"></i></div><i class="iconfont icon-play"></i><i class="iconfont icon-stop" style="display: none;"></i><div class="right"><i class="iconfont icon-x"></i><div class="alltime">1:00</div></div></div><div class="longarr"></div><style type="text/css" class="css"></style></div>';
+//		ele.innerHTML=hmele;
 		hm=new Object;
 		hm.e=new Object;
 		hm.nowduan=0;
@@ -25,6 +188,8 @@ function Hmusic(ele,arr){
 		hm.e.nowtime=$c('.nowtime');
 		hm.e.alltime=$c('.alltime');
 		hm.e.lrc=$c('.lrc');
+		hm.e.x1=$c('.icon-xunhuan');
+		hm.e.x2=$c('.icon-ttpodicon');
 		hm.e.longarr=$c('.longarr');
 		hm.e.title.innerHTML=hm.p[hm.nowduan].title;
 		function getalltime(){
@@ -76,16 +241,19 @@ function Hmusic(ele,arr){
 		//定时器 10
 		hm.interval=function(){
 			var t=parseFloat(hm.e.audio.currentTime.toFixed(2));
-			for(var i=0;i<hm.lrc.b.length;i++){
-				if(hm.lrc.b[i]==t){
-					var t2=70-i*30;
-					hm.e.lrc.style.transform='translateY('+t2+'px)';
-					hm.e.lrc.style.webkitTransform='translateY('+t2+'px)';
-					hm.e.lrc.style.MozTransform='translateY('+t2+'px)';
-					$c('.css').innerHTML='.lrc>p:nth-child('+(1+i)+'){font-size:24px}';
-					break;
+			if(hm.lrc){
+				for(var i=0;i<hm.lrc.b.length;i++){
+					if(hm.lrc.b[i]==t){
+						var t2=70-i*30;
+						hm.e.lrc.style.transform='translateY('+t2+'px)';
+						hm.e.lrc.style.webkitTransform='translateY('+t2+'px)';
+						hm.e.lrc.style.MozTransform='translateY('+t2+'px)';
+						$c('.css').innerHTML='.lrc>p:nth-child('+(1+i)+'){font-size:24px!important}';
+						break;
+					}
 				}
 			}
+
 		}
 		setInterval(hm.interval,10);
 		function getvtime(time) {
@@ -207,4 +375,21 @@ function Hmusic(ele,arr){
 		hm.e.btns.addEventListener('click',function(){
 			hm.huan(hm.nowduan-1);
 		})
+		hm.e.audio.onended=function(){
+			console.log('播放完成');
+			if(hm.e.x2.style.display=='none'){
+				hm.huan(hm.nowduan+1);
+			}else{
+				hm.e.audio.play();
+			}
+		}
+		hm.e.x1.addEventListener('click',function(){
+			this.style.display='none';
+			hm.e.x2.style.display='block';
+		})
+		hm.e.x2.addEventListener('click',function(){
+			this.style.display='none';
+			hm.e.x1.style.display='block';
+		})
+	
 }
