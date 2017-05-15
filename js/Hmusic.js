06060dcbaa562fdf3b8896765347f4411f2f5779
皓东haocity <ureygt@gmail.com>
2017-05-15 19:18:59 +0800

    剔除掉ajax和云单曲的函数    修复gettop的bug

--- js/Hmusic.js
+++ js/Hmusic.js
@@ -1,4 +1,4 @@
-var e='.warp{margin:0 auto;width:340px}.hmusic{font-size: 16px;height:500px;box-shadow:0 0 5px rgba(0,0,0,.8);font-family:Avenir Next,Helvetica,Arial,Lantinghei SC,Microsoft YaHei,sans-serif}.hmusic>.banner{height:200px;background-image:url(1.jpg);background-position:center;background-size:cover}.hmusic>.banner>.banner-zz{overflow:hidden;height:100%;background-color:rgba(0,0,0,.4);color:#fff}.hmusic>.banner>.banner-zz>.songname{padding-top:5px;height:25px;text-align:center;font-size:1.2em;white-space:nowrap}.hmusic>.banner>.banner-zz>.lrc-warp{overflow:hidden;height:10pc}.hmusic>.banner>.banner-zz>.lrc-warp>.lrc{text-align:center;transition:all .3s ease-out;transform:translateY(-10px)}.hmusic>.banner>.banner-zz>.lrc-warp>.lrc>p{white-space: nowrap;margin:0;padding:0;line-height:30px;font-size:12px}.hmusic>.banner>.banner-zz>.lrc-warp>.lrc>.nowlrcp{font-size:24px;white-space: inherit;}.hmusic>.banner>.banner-zz>.ranger{cursor: pointer;height:9pt;background-color:rgba(59,65,67,.39)}.hmusic>.banner>.banner-zz>.ranger>.ranger-n{width:20%;height:100%;background-color:rgba(3,169,244,.6)}.hmusic>.con{height:40px;text-align:center;user-select:none}.hmusic>.con .sound-ranger{height:80px;width:16px;background-color:rgba(255,255,255,.4);cursor:pointer;display:none}.hmusic>.con .sound-ranger>.sound-ranger-a{height:100%}.hmusic>.con .sound-ranger>.sound-ranger-a>.sound-ranger-b{height:82px;position:absolute;width:100%;bottom:18px;background-color:#6D95E0}.hmusic>.con .iconfont{color:#6d95e0;font-size:1.6em;cursor:pointer}.hmusic>.con .iconfont:hover{color:#2753af}.hmusic>.con>.icon-play,.hmusic>.con>.icon-stop{position:relative;font-size:2em;line-height:40px;}.hmusic>.con>.right{float:right;padding:0 4px;position:relative}.hmusic>.con>.left{float:left;padding:0 4px;position:relative}.hmusic>.con>.left>.nowtime,.hmusic>.con>.right>.alltime{position:relative;top:-1pc;display:inline-block;font-size:.6em}.hmusic>.con>.right>.icon-x{padding-right:30px;line-height:1.6em}.hmusic>.con>.left>.icon-s{padding-left:30px;line-height:1.6em}.hmusic>.con .sound{position:absolute;bottom:2px;right:24px;z-index:10}.hmusic>.con .sound>.icon-yinliang{font-size:1em}.hmusic>.con .xunhuan{font-size:.9em;position:absolute;bottom:1px;right:4px}.hmusic>.longarr{width:100%;overflow-x:hidden;overflow-y:auto;height:260px;background-color:#fafafa}.hmusic>.longarr>li{white-space:nowrap;padding:3px 0 3px 20px;cursor:pointer}.hmusic>.longarr>li:hover{padding:3px 0 3px 24px;background-color:hsla(0,0%,69%,.9)}';
+var e='.warp{margin:0 auto;width:340px}.hmusic{font-size: 16px;height:500px;box-shadow:0 0 5px rgba(0,0,0,.8);font-family:Avenir Next,Helvetica,Arial,Lantinghei SC,Microsoft YaHei,sans-serif}.hmusic>.banner{height:200px;background-image:url(1.jpg);background-position:center;background-size:cover}.hmusic>.banner>.banner-zz{overflow:hidden;height:100%;background-color:rgba(0,0,0,.4);color:#fff}.hmusic>.banner>.banner-zz>.songname{padding-top:5px;height:25px;text-align:center;font-size:1.2em;white-space:nowrap}.hmusic>.banner>.banner-zz>.lrc-warp{overflow:hidden;height:10pc}.hmusic>.banner>.banner-zz>.lrc-warp>.lrc{text-align:center;transition:all .3s ease-out;transform:translateY(-10px)}.hmusic>.banner>.banner-zz>.lrc-warp>.lrc>p{white-space: nowrap;margin:0;padding:0;line-height:30px;font-size:12px}.hmusic>.banner>.banner-zz>.lrc-warp>.lrc>.nowlrcp{font-size:24px;white-space: inherit;}.hmusic>.banner>.banner-zz>.ranger{cursor: pointer;height:9pt;background-color:rgba(59,65,67,.39)}.hmusic>.banner>.banner-zz>.ranger>.ranger-n{width:20%;height:100%;background-color:rgba(3,169,244,.6)}.hmusic>.con{height:40px;text-align:center;user-select:none}.hmusic>.con .sound-ranger{height:80px;width:16px;background-color:rgba(255,255,255,.4);cursor:pointer;display:none}.hmusic>.con .sound-ranger>.sound-ranger-a{height:100%;position:relative}.hmusic>.con .sound-ranger>.sound-ranger-a>.sound-ranger-b{height:82px;position:absolute;width:100%;bottom:0;background-color:#6D95E0}.hmusic>.con .iconfont{color:#6d95e0;font-size:1.6em;cursor:pointer}.hmusic>.con .iconfont:hover{color:#2753af}.hmusic>.con>.icon-play,.hmusic>.con>.icon-stop{position:relative;font-size:2em;line-height:40px;}.hmusic>.con>.right{float:right;padding:0 4px;position:relative}.hmusic>.con>.left{float:left;padding:0 4px;position:relative}.hmusic>.con>.left>.nowtime,.hmusic>.con>.right>.alltime{position:relative;top:-1pc;display:inline-block;font-size:.6em}.hmusic>.con>.right>.icon-x{padding-right:30px;line-height:1.6em}.hmusic>.con>.left>.icon-s{padding-left:30px;line-height:1.6em}.hmusic>.con .sound{position:absolute;bottom:2px;right:24px;z-index:10}.hmusic>.con .sound>.icon-yinliang{font-size:1em}.hmusic>.con .xunhuan{font-size:.9em;position:absolute;bottom:1px;right:4px}.hmusic>.longarr{width:100%;overflow-x:hidden;overflow-y:auto;height:260px;background-color:#fafafa}.hmusic>.longarr>li{white-space:nowrap;padding:3px 0 3px 20px;cursor:pointer}.hmusic>.longarr>li:hover{padding:3px 0 3px 24px;background-color:hsla(0,0%,69%,.9)}';
 var e2= document.createElement('style');
 e2.type="text/css";
 e2.innerHTML=e;
@@ -9,81 +9,16 @@ e3.type='text/css';
 e3.rel='stylesheet';
 e3.href=fontcss;
 document.head.appendChild(e3);
-//ajax
-var gc = gc || {};
-gc.ajax = function(options){//options =  {url:'',method:'',data:'',success:'',async:''}
-    //默认参数
-    options.url = options.url || '',
-    options.method = options.method || 'get',
-    options.data = options.data || '',
-    options.success = options.success || '',
-    options.error = options.error || function(e){console.log(e)},
-    options.async = options.async || true;
-    //get请求-拼接url
-    console.log(options.method.toLowerCase());
-    if(options.method.toLowerCase() == 'get'){
-        if(typeof options.data == 'object'){
-            options.data = [];
-            for (var k in options.data){
-                options.data.push(k+'='+options.data[k]);
-                options.data.join('&');
-            }
-        }
-        options.url += (options.url.indexOf('?' == -1) ? '?' : '') + options.data;
-    }
-    //post请求-转换字符串
-    if(options.method.toLowerCase() == 'post'){
-        if(typeof options.data == 'object'){
-            var arrs = [];
-            for (var k in options.data){
-                arrs.push(k+'='+options.data[k]);
-            }
-            options.data = arrs.join('&');
-        }
-    }
-    //创建发送请求
-    var xhr = window.XMLHttpRequest ?  new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP'); //兼容ie
-    xhr.open(options.method,options.url,options.async);
-    if(options.method == 'post'){
-        xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
-        xhr.send(options.data);
-    }else{
-        xhr.send(null);
-    }
-    //异步请求
-    if(options.async == true){
-        xhr.onreadystatechange = function(){
-          if(xhr.readyState == 4){
-              callcall();
-          }
-        }
-    }
-    // xhr.abort(); // 取消异步请求
-    //同步请求
-    if(options.async == false){
-        callcall();
-    }
-    //返回状态判断
-    function callcall(){
-        if(xhr.status == 200){
-            options.success(xhr.responseText);
-        }else{
-            options.error('error:' + xhr.status + xhr.statusText);
-        }
-    }
-};
-
-
 function hyplaylist(ele,id){
 	var yl=new Object;
 	yl.ele=ele;
 	yl.arr=new Array;
-	 var api='https://api.imjad.cn/cloudmusic';
-	 gc.ajax({
-	        url:api+'/?type=playlist&id='+id+'&br=128000',
-	        success:function(res){
-	            t=JSON.parse(res).playlist.tracks;
-	            for (var i = 0; i < t.length; i++) {
+	var api='https://api.imjad.cn/cloudmusic';
+	var	xmlhttp=new XMLHttpRequest();
+	xmlhttp.onreadystatechange=function(){
+		if (xmlhttp.readyState==4 && xmlhttp.status==200){
+			var t=JSON.parse(xmlhttp.responseText).playlist.tracks;
+			  for (var i = 0; i < t.length; i++) {
 	            	var c=new Object;
 	            	var au='';
 	            	c.yunid=t[i].id;
@@ -102,75 +37,13 @@ function hyplaylist(ele,id){
 	            }
 	            console.log('加载歌单成功 正在解析各个音频地址');
 	            Hmusic(yl.ele,yl.arr);
-	        }
-	    });
-	
-}
 
-function hyunmusic(ele,arr,one){
-	var ym=new Object;
-	ym.arr=arr;
-	ym.ele=ele;
-	ym.yuncloud=function(arr){
-	    for (var i = 0; i < arr.length; i++) {
-	       ym.getmusicutl(arr[i].yunid,i,one)
-	    };
-	}
-	ym.getmusicutl=function(id,i,one){
-	    var api='https://api.imjad.cn/cloudmusic';
-	    ym.arr[i].lrc=api+'/?type=lyric&id='+id+'&br=128000';
-	    gc.ajax({
-	    url:api+'/?type=song&id='+id+'&br=128000',
-	    success:function(res){
-	        t=JSON.parse(res).data[0].url;
-	        if(!ym.arr[i].audio){ym.arr[i].audio=t}
-	        ym.checksuccess(ym.arr);
-	    },
-	    error:function(e){
-	    	console.log(e);
-	    }
-	    });
-	    if(!one){
-	    	gc.ajax({
-	        url:api+'/?type=detail&id='+id+'&br=128000',
-	        success:function(res){
-	            t=JSON.parse(res).songs[0];
-	            var au='';
-	            if(t.ar.length>4){
-	               au='群星'
-	            }else{
-	                for (var x = 0; x < t.ar.length; x++) {
-	                    au+=t.ar[x].name
-	                }
-	            }
-	            ym.arr[i].img=t.al.picUrl+'?param=320y320';
-	            ym.arr[i].au=au;
-	            if(!ym.arr[i].title){
-	            	ym.arr[i].title=t.name+'-'+ym.arr[i].au;
-	            }
-	            ym.checksuccess(ym.arr);
-	        },
-	        error:function(e){
-	    	console.log(e);
-	    	}
-		    });
-	    }
-
-	}
-	ym.checksuccess=function(arr){
-		var t=0;
-		for (var i = 0; i < arr.length; i++) {
-			if(arr[i].img&&arr[i].audio&&arr[i].title) {t++;}
-		}
-		if(t==arr.length){
-			console.log('yun music success');
-			Hmusic(ym.ele,arr)
-		}else{
-			console.log('没有完成');
 		}
 	}
-	ym.yuncloud(ym.arr);
+	xmlhttp.open("GET",api+'/?type=playlist&id='+id+'&br=128000',true);
+	xmlhttp.send();
 }
+
 
 function Hmusic(ele,arr){
 		hm=new Object;
@@ -182,7 +55,7 @@ function Hmusic(ele,arr){
 		hm.nowduan=0;
 		hm.volume=1;
 		hm.nowlrc=-1;
-		hm.p=arr;//[{"title":"ACFUN次元之旅","audio":"1.wav","img":"1.jpg","lrc":"1.json"},{"title":"おちゃめ机能 -Full","audio":"2.mp3","img":"2.jpg","lrc":"2.json"}]
+		hm.p=arr;
 		hm.e.audio=$c('.hmusic>.hm-audio');
 		hm.e.banner=$c('.banner');
 		hm.e.btnplay=$c('.icon-play');
@@ -216,19 +89,27 @@ function Hmusic(ele,arr){
 			hm.play();
 		});
 		hm.e.btnstop.addEventListener('click',function(){
-			hm.play();
+			hm.pause();
 		});
+
 		hm.play=function(){
+			hm.e.btnplay.style.display='none';
+			hm.e.btnstop.style.display='inline-block';
+			if(hm.e.audio.paused){
+				hm.e.audio.play()
+			}
+		}
+		hm.pause=function(){
+			hm.e.btnplay.style.display='inline-block';
+			hm.e.btnstop.style.display='none';
+			hm.e.audio.pause();
+		}
+
+		hm.playswitch=function(){
 			if(hm.e.btnplay.style.display!=='none'){
-				hm.e.btnplay.style.display='none';
-				hm.e.btnstop.style.display='inline-block';
-				hm.e.audio.play();
-				console.log('play')
+				hm.play()
 			}else{
-				hm.e.btnplay.style.display='inline-block';
-				hm.e.btnstop.style.display='none';
-				hm.e.audio.pause();
-				console.log('pause')
+				hm.pause()
 			}
 		}
 		
@@ -276,10 +157,10 @@ function Hmusic(ele,arr){
 	        };
 	    }
 		function show_coords(event, elem) {
-	        var x = event.clientX - getLeft(elem);
-	        var y = event.clientY - getTop(elem);
+	        var x = event.clientX - getLeft(elem)+window.scrollX;
+	        var y = event.clientY - getTop(elem)+window.scrollY;
 	        var xbl = x / elem.offsetWidth;
-	        var ybl =1- y / elem.offsetHeight;
+	        var ybl =1- y /elem.offsetHeight;
 	        return {
 	            x:x,
 	            y:y,
@@ -307,8 +188,11 @@ function Hmusic(ele,arr){
 					if (xmlhttp.readyState==4 && xmlhttp.status==200){
 						var t=JSON.parse(xmlhttp.responseText);
 						hm.e.audio.src=t.data[0].url;
-						if(!stop){hm.e.audio.play();}
-						callback();
+						if(!stop&&hm.e.audio.paused){hm.e.audio.play();}
+						if (typeof callback === "function"){
+					        callback()
+					    }
+						
 					}
 				}
 	    		xmlhttp.open("GET",url,true);
@@ -382,10 +266,10 @@ function Hmusic(ele,arr){
 	    		hm.e.lrc.style.transform='translateY(60px)';
 	    		hm.e.title.innerHTML=hm.p[hm.nowduan].title;
 	    		if(hm.p[hm.nowduan].yunid){
-	    			hm.getcloudurl('https://api.imjad.cn/cloudmusic/?type=song&id='+hm.p[hm.nowduan].yunid+'&br=128000',stop,hm.huan2);
+	    			hm.getcloudurl('https://api.imjad.cn/cloudmusic/?type=song&id='+hm.p[hm.nowduan].yunid+'&br=128000',stop,hm.huan2(stop));
 	    		}else{
 	    			hm.e.audio.src=hm.p[hm.nowduan].audio;
-	    			hm.huan2();
+	    			hm.huan2(stop);
 	    		}
 				var li=hm.e.longarr.querySelectorAll('li');
 				for (var i = 0; i < li.length; i++) {
@@ -415,10 +299,10 @@ function Hmusic(ele,arr){
 				hm.huan(this.songid);
 			})
 	   	}
-	   	hm.huan2=function(){
+	   	hm.huan2=function(stop){
 	   		hm.e.banner.style.backgroundImage='url('+hm.p[hm.nowduan].img+')';
 			hm.getlrc(hm.p[hm.nowduan].lrc);
-			if(!stop){hm.e.audio.play();
+			if(!stop&&hm.e.audio.paused){hm.e.audio.play();
 			hm.e.btnplay.style.display='none';
 			hm.e.btnstop.style.display='inline-block';
 			}
@@ -446,7 +330,7 @@ function Hmusic(ele,arr){
 			this.style.display='none';
 			hm.e.x1.style.display='block';
 		})
-		hm.e.sounda.addEventListener('click',function(event){
+		hm.e.wsound.addEventListener('click',function(event){
 			var e = event || window.event || arguments.callee.caller.arguments[0];
 			var bl = show_coords(e,this);
 			hm.e.soundb.style.height=bl.h*bl.ybl+'px';
